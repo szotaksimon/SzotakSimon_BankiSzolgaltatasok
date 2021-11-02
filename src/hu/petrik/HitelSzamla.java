39868772682860f60dc7c1d6ae93c8fa029423c0
Szoták Simon <szotak.simon16@gmail.com>
2021-11-02 17:02:39 +0100

    HitelSzamla

--- src/hu/petrik/HitelSzamla.java
+++ src/hu/petrik/HitelSzamla.java
@@ -1,4 +1,24 @@
 package hu.petrik;
 
-public class HitelSzamla {
+public class HitelSzamla extends Szamla {
+    private final int hitelKeret;
+
+    public HitelSzamla(Tulajdonos tulajdonos, int hitelKeret) {
+        super(tulajdonos);
+        this.hitelKeret = hitelKeret;
+    }
+    public int getHitelKeret() {
+        return hitelKeret;
+    }
+
+    @Override
+    public boolean kivesz(double osszeg) {
+        boolean sikeres = true;
+        if (osszeg > getAktualisEgyenleg() + hitelKeret) {
+            sikeres = false;
+        } else {
+            setAktualisEgyenleg(getAktualisEgyenleg() - osszeg);
+        }
+        return sikeres;
+    }
 }
