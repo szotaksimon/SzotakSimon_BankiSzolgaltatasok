c67d7b7e8df785236c2ce975575c080da5fc0f5a
Szoták Simon <szotak.simon16@gmail.com>
2021-11-02 16:52:41 +0100

    Tulajdonos és BankiSzolgaltatas osztályok létrehozása

---
+++ src/hu/petrik/BankiSzolgaltatas.java
@@ -0,0 +1,13 @@
+package hu.petrik;
+
+public class BankiSzolgaltatas {
+    private final Tulajdonos tulajdonos;
+
+    public BankiSzolgaltatas(Tulajdonos tulajdonos) {
+        this.tulajdonos = tulajdonos;
+    }
+
+    public Tulajdonos getTulajdonos() {
+        return tulajdonos;
+    }
+}
